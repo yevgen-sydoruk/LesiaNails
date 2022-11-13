59009e99083138ae213790480b380f22be8b7d9c
yevgen-sydoruk <61704998+yevgen-sydoruk@users.noreply.github.com>
2022-11-13 15:18:53 -0600

    Testing form

--- js/script.js
+++ js/script.js
@@ -1,3 +1,75 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("form");
+    form.addEventListener("submit", formSend);
+
+    async function formSend(e) {
+        e.preventDefault();
+
+        let error = formValidate(form);
+
+        let formData = new FormData(form);
+        // formData.append('image', formImage.files[0]);
+
+        if (error === 0) {
+            form.classList.add("_sending");
+            let response = await fetch("sendmail.php", {
+                method: "POST",
+                body: formData,
+            });
+
+            if (resposse.ok) {
+                let result = await response.json();
+                alert(result.message);
+                formPreview.innerHTML = "";
+                form.reset();
+                form.classList.remove(_sending);
+            } else {
+                alert("Error");
+                form.classList.remove(_sending);
+            }
+        } else {
+            alert("Fields must be filled correctly");
+        }
+    }
+
+    function formValidate(form) {
+        let error = 0;
+        let formReq = document.querySelectorAll("._req");
+
+        for (let index = 0; index < formReq.length; index++) {
+            const input = formReq[index];
+            formRemoveError(input);
+
+            if (input.classList.contains("_email")) {
+                if (emailTest(input)) {
+                    formAddError(input);
+                    error++;
+                }
+            } else {
+                if (input.value === "") {
+                    formAddError(input);
+                    error++;
+                }
+            }
+        }
+        return error;
+    }
+    function formAddError(input) {
+        input.parentElement.classList.add("_error");
+        input.classList.add("_error");
+    }
+    function formRemoveError(input) {
+        input.parentElement.classList.remove("_error");
+        input.classList.remove("_error");
+    }
+    //email checker
+    function emailTest(input) {
+        return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+    }
+});
+
 // Burger Toggel
 const nav = document.querySelector(".header__nav");
 const burger = document.querySelector("#burger");
---
+++ sendmail.php
@@ -0,0 +1,50 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require "phpmailer/src/Exception.php";
+require "phpmailer/src/PHPMailer.php";
+
+$mail = new PHPMailer(true);
+$mail->CharSet = "UTF-8";
+$mail->setLanguage("ru", "phpmailer/language/");
+$mail->IsHTML(true);
+
+$mail->setFrom("drevil3372@gmail.com", "Nails");
+$mail->addAddress("drevil3372@gmail.com", "yevgen.sydoruk@gmail.com");
+$mail->Subject = "Nailllls!";
+
+//mail body
+$body = "<h1>New mail</h1>";
+
+if (trim(!empty($_POST["name"]))) {
+    $body .= "<p><strong>Name:</strong> " . $_POST["name"] . "</p>";
+}
+if (trim(!empty($_POST["email"]))) {
+    $body .= "<p><strong>Email:</strong> " . $_POST["email"] . "</p>";
+}
+if (trim(!empty($_POST["phone"]))) {
+    $body .= "<p><strong>Phone:</strong> " . $_POST["phone"] . "</p>";
+}
+if (trim(!empty($_POST["date"]))) {
+    $body .= "<p><strong>Date:</strong> " . $_POST["date"] . "</p>";
+}
+if (trim(!empty($_POST["message"]))) {
+    $body .= "<p><strong>Message:</strong> " . $_POST["message"] . "</p>";
+}
+
+$mail->Body = $body;
+
+//sending
+if (!$mail->send()) {
+    $message = "Error";
+} else {
+    $message = "Mail sent";
+}
+
+$response = ["message" => $message];
+
+header("Content-type: application/json");
+echo json_encode($response);
+
+?>
